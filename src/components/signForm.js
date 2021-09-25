f6b80f7bd5d2ca67e98d8ce75947aeea34d71af3
Henok <henatan99@gmail.com>
2021-09-25 19:56:56 +0300

    Add defaultProps and PropTypes to the SignForm component

--- src/components/signForm.js
+++ src/components/signForm.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const SignForm = (username, password, handleUsernameChange, handlePasswordChange, handleClick) => {
     <form>
         <input value={username} onChange={handleUsernameChange} placeholder="Username or email"/>
-        <input value={password} onChange={handlePasswordChange} placeholder="Password"/>
+        <input type="password" value={password} onChange={handlePasswordChange} placeholder="Password"/>
         <button type="button" onClick={handleClick}>Submit</button>
     </form>
+}
+
+SignForm.defaultProps = {
+    username: '',
+    password: '',
+    handleUsernameChange: null,
+    handlePasswordChange: null,
+    handleClick: null,
+}
+
+SignForm.propTypes = {
+    username: PropTypes.string,
+    password: PropTypes.string,
+    handleUsernameChange: PropTypes.func,
+    handlePasswordChange: PropTypes.func,
+    handleClick: PropTypes.func
 }
 
 export default SignForm;
