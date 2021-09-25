0dd369b2a43cb843da9d062ae6907723011733ae
Henok <henatan99@gmail.com>
2021-09-25 20:40:32 +0300

    Add MessagePage component

---
+++ src/containers/messagePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MessageSection from './messageSection';
+
+const MessagePage = () => {
+    return (
+        <div>
+            <MessageSection />
+        </div>
+    )
+}
+
+export default MessageSection;
