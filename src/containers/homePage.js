06ad341ee8b550773eb92e62cb71d21978daf514
Henok <henatan99@gmail.com>
2021-09-25 20:31:30 +0300

    Add HomePage component

---
+++ src/containers/homePage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import HomeNav from '../components/homeNav';
+
+const HomePage = () => (
+    <div>
+        <HomeNav />
+        <main>
+            <section>
+                <h1>Welcome to ChatRoom</h1>
+            </section>
+        </main>
+    </div>
+)
+
+export default HomePage;
