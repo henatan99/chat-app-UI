b45ba84c7a1a6e42ae6e9b72f32f69773b9d2c30
Henok <henatan99@gmail.com>
2021-09-25 19:52:19 +0300

    Add SignPage component

--- src/components/signForm.js
+++ src/components/signForm.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
-const SignForm = (sign) => {
+const SignForm = (username, password, handleUsernameChange, handlePasswordChange, handleClick) => {
     <form>
         <input value={username} onChange={handleUsernameChange} placeholder="Username or email"/>
         <input value={password} onChange={handlePasswordChange} placeholder="Password"/>
-        <button type="button" onClick={sign === 'signup' ? handleSignupClick : handlSigninClick}>Submit</button>
+        <button type="button" onClick={handleClick}>Submit</button>
     </form>
 }
+
+export default SignForm;
--- src/containers/SignPage.js
+++ src/containers/SignPage.js
@@ -1,2 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+import SignForm from '../components/signForm';
 
+const SignPage = (sign) => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleUsernameChange = e => setUsername(e.target.value);
+    const handlePasswordChange = e => setPassword(e.target.value);
+    const handleClick = (e) => {
+        e.preventDefault();
+    }
+
+    return(
+        <div>
+            <SignForm username={username} password={password} handleUsernameChange={handleUsernameChange} handlePasswordChange={handlePasswordChange} handleClick={handleClick} />
+        </div>
+    )
+}
+
+export default SignPage;
