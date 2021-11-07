a1a38a948b942faa0ba579741faee1c5d29e4a34
yocosaka <sakayoco55@gmail.com>
2021-11-07 11:34:54 +0900

    Update production's baseApi path

--- src/helpers/baseApi.js
+++ src/helpers/baseApi.js
@@ -1,4 +1,4 @@
 const baseApi = 'http://localhost:3001/api/v1/';
-// const baseApi = 'https://chat-app-api-dream-mv.herokuapp.com/api/v1/';
+// const baseApi = 'https://chat-app-api-dreamteam.herokuapp.com/api/v1/';
 
 export default baseApi;
